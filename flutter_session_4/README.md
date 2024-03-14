d368f4d324de100460d41e8b5c4777fbb6c515a0
Yasha Gozwan Shuhada <yashagozwan13@gmail.com>
2024-03-14 10:20:07 +0700

    flutter session 4 start

---
+++ flutter_session_4/README.md
@@ -0,0 +1,7 @@
+# Flutter Session 4
+
+## Flutter SDK
+
+```plaintext
+flutter: 3.13.7
+```
---
+++ flutter_session_4/android/app/src/main/kotlin/com/yashagozwan/flutter_session_4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yashagozwan.flutter_session_4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
